8614824e7bfc2019d0b06ea8d1dee7ea581c9899
Aman Baldha <169175825+amanbaldha@users.noreply.github.com>
2024-08-13 21:44:44 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,24 @@
+public class prime {
+public static void main(String[] args) {
+        // Check if a number is prime
+        int number = 4; // You can change this number to test different values
+        boolean isPrime = true;
+        
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        
+        if (isPrime) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+}
+}
